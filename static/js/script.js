829e3f446a825cf0456661735c7c51fdcbc739c8
Franciskadtt <franciskadtt@gmail.com>
2021-06-04 11:24:04 +0000

    Add resource form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,16 @@
-// Sidenav Open and close
+
 $(document).ready(function () {
+    // Sidenav Open and close
     $('.sidenav').sidenav({edge: "right"});
+    // Date Picker
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
+
+
