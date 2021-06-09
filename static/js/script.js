ee9e28ba4de88901553a3bd42296cbd18f82ef1c
Franciskadtt <franciskadtt@gmail.com>
2021-06-09 11:36:45 +0000

    Style navbar and index.html

--- static/js/script.js
+++ static/js/script.js
@@ -41,3 +41,21 @@ $(document).ready(function () {
         });
     }
 });
+
+// From https://www.infoworld.com/article/3304440/setting-an-active-menu-item-based-on-the-current-url-with-jquery.html
+$(function () {
+    setNavigation();
+});
+
+function setNavigation() {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+
+    $("li a").each(function () {
+        var href = $(this).attr('href');
+        if (path.substring(0, href.length) === href) {
+            $(this).closest('li').addClass('active');
+        }
+    });
+}
