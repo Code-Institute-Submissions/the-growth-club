f011685b082b4e1f60c299308c40d8dc83a9c2dc
Franciskadtt <franciskadtt@gmail.com>
2021-06-29 14:15:24 +0000

    Add scroll up button to the resources page

--- static/js/script.js
+++ static/js/script.js
@@ -73,3 +73,27 @@ function setNavigation() {
         }
     });
 }
+
+// PAGE SCROLL UP FEATURE
+/** Add scroll up function
+orginal code from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp with modifications for project
+*/
+//Get the button
+var scrollUpButton = document.getElementById("scrollButton");
+    // When the user scrolls down 40px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction(); 
+    };
+    // To set the paramaters for the scroll function
+    function scrollFunction() {
+        if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        scrollUpButton.style.display = "block";
+        } else {
+        scrollUpButton.style.display = "none";
+        }
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
