f44b8e3555d54460406768a98f79f5cf8653aa3f
Franciskadtt <franciskadtt@gmail.com>
2021-07-08 13:51:47 +0000

    Install Flask-SSLify 0.1.5 flask extension to ensure links are secure

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
+from flask_sslify import SSLify
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+sslify = SSLify(app)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
@@ -711,4 +713,4 @@ def index():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)  # update this to debug=False before submission
+            debug=False)  # update this to debug=False before submission
