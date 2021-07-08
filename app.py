66f10ee24db10e163ed7f7855c345c79fd50cc36
Franciskadtt <franciskadtt@gmail.com>
2021-07-08 13:29:50 +0000

    Install talisman flask extension to ensure links are secure

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
+from flask_talisman import Talisman
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,6 +10,7 @@ if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+Talisman(app)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
