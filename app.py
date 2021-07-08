5963ed158a86cbd9091d5e3c1241a5d36dc625a2
Franciskadtt <franciskadtt@gmail.com>
2021-07-08 13:32:55 +0000

    Remove talisman flask extension from app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
-from flask_talisman import Talisman
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -10,7 +9,6 @@ if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
-Talisman(app)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
